f76c1a9d6beb91ec570504bf93f37d033edbdc97
Ricardo <richago29@gmail.com>
2019-11-07 00:38:11 -0500

    ejemplo efectivo de querys y base de datos terminada, empezar modelado con django

--- sqlDatabase.sql
+++ sqlDatabase.sql
@@ -11,7 +11,7 @@ create table usuarios (
 create table ordenes(
     id_orden SERIAL primary key,
     id_usuario SERIAL not null,
-    fecha datetime not null,
+    fecha timestamp not null,
     direccion varchar(64) not null,
     constraint fk_usuario foreign key (id_usuario) references usuarios(id_usuario)
 );
@@ -31,7 +31,8 @@ create table tamano(
     nombre varchar(32) not null
 );
 
-create table producto_tamano_subtipo(
+create table pro_tam_sub(
+    id_pts serial primary key,
     id_subtipo serial,
     id_tamano serial,
     id_producto serial not null,
@@ -54,21 +55,17 @@ create table toppings(
 
 create table prodorden(
     id_productocreado SERIAL primary key,
-    id_producto SERIAL not null,
-    id_tamano SERIAL,
+    id_pts serial not null,
     id_orden serial not null,
-    id_subtipo SERIAL not null,
     cantidad integer not null,
-    constraint fk_creado foreign key (id_producto) references producto(id_producto),
-    constraint fk_tamano foreign key (id_tamano) references tamano(id_tamano),
-    constraint fk_subtipo foreign key (id_subtipo) references subtipo(id_subtipo),
+    constraint fk_pts foreign key (id_pts) references prod_tam_sub(id_pts), 
     constraint fk_orden foreign key (id_orden) references ordenes(id_orden)
 );
 
 create table prodorden_topping(
     id_productocreado SERIAL not null,
     id_topping SERIAL not null,
-    constraint fk_productoCreado foreign key (id_productoCreado) references prodorden(id_productocreado),
+    constraint fk_productoCreado foreign key (id_productocreado) references prodorden(id_productocreado),
     constraint fk_topping foreign key (id_topping) references toppings(id_topping) 
 );
 
@@ -90,15 +87,6 @@ insert into producto (tipo) values ('Pizza');
 insert into producto (tipo) values ('Dinner Plates');
 insert into producto (tipo) values ('Drinks');
 
-insert into subtipo (nombre) values ('Regular 0 toppings');
-insert into subtipo (nombre) values ('Sicilian 0 toppings');
-insert into subtipo (nombre) values ('Regular 1 toppings');
-insert into subtipo (nombre) values ('Sicilian 1 toppings');
-insert into subtipo (nombre) values ('Regular 2 toppings');
-insert into subtipo (nombre) values ('Sicilian 2 toppings');
-insert into subtipo (nombre) values ('Regular 3 toppings');
-insert into subtipo (nombre) values ('Sicilian 3 toppings');
-
 insert into subtipo (nombre) values ('Regular');
 insert into subtipo (nombre) values ('Cheese');
 insert into subtipo (nombre) values ('italian');
@@ -113,6 +101,15 @@ insert into subtipo (nombre) values ('Steak');
 insert into subtipo (nombre) values ('Steak + Cheese');
 insert into subtipo (nombre) values ('Steak + Mushrooms');
 
+insert into subtipo (nombre) values ('Regular 0 toppings');
+insert into subtipo (nombre) values ('Sicilian 0 toppings');
+insert into subtipo (nombre) values ('Regular 1 toppings');
+insert into subtipo (nombre) values ('Sicilian 1 toppings');
+insert into subtipo (nombre) values ('Regular 2 toppings');
+insert into subtipo (nombre) values ('Sicilian 2 toppings');
+insert into subtipo (nombre) values ('Regular 3 toppings');
+insert into subtipo (nombre) values ('Sicilian 3 toppings');
+
 insert into tamano (nombre) values ('small');
 insert into tamano (nombre) values ('large');
 insert into tamano (nombre) values ('pizza slice');
@@ -124,37 +121,37 @@ insert into ordenes (id_usuario,fecha,direccion) values (1,now(),'muy lejos');
 insert into ordenes (id_usuario,fecha,direccion) values (2,now(),'muy lejos');
 insert into ordenes (id_usuario,fecha,direccion) values (1,now(),'muy lejos');
 
+insert into pizza (num_toppings,id_subtipo) values (0,14);
 insert into pizza (num_toppings,id_subtipo) values (0,15);
-insert into pizza (num_toppings,id_subtipo) values (0,16);
+insert into pizza (num_toppings,id_subtipo) values (1,16);
 insert into pizza (num_toppings,id_subtipo) values (1,17);
-insert into pizza (num_toppings,id_subtipo) values (1,18);
+insert into pizza (num_toppings,id_subtipo) values (2,18);
 insert into pizza (num_toppings,id_subtipo) values (2,19);
-insert into pizza (num_toppings,id_subtipo) values (2,20);
+insert into pizza (num_toppings,id_subtipo) values (3,20);
 insert into pizza (num_toppings,id_subtipo) values (3,21);
-insert into pizza (num_toppings,id_subtipo) values (3,22);
 
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,15,1,8.35);
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,17,1,9.50);
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,19,1,10.50);
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,21,1,11.70);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,14,1,8.35);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,16,1,9.50);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,18,1,10.50);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,20,1,11.70);
 
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,15,2,13.75);
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,17,2,15.75);
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,19,2,17.75);
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,21,2,19.75);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,14,2,13.75);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,16,2,15.75);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,18,2,17.75);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,20,2,19.75);
 
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,16,1,16.75);
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,18,1,18.75);
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,20,1,19.75);
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,22,1,20.75);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,15,1,16.75);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,17,1,18.75);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,19,1,19.75);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,21,1,20.75);
 
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,16,2,26.50);
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,18,2,28.50);
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,20,2,30.50);
-insert into producto_tamano_subtipo (id_producto,id_subtipo,id_tamano,precio) values (3,22,2,31.50);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,15,2,26.50);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,17,2,28.50);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,19,2,30.50);
+insert into pro_tam_sub (id_producto,id_subtipo,id_tamano,precio) values (3,21,2,31.50);
 
-insert into prodorden (id_orden,id_producto,id_subtipo,id_tamano,cantidad) values (1,3,17,1,1);
-insert into prodorden (id_orden,id_producto,id_subtipo,id_tamano,cantidad) values (1,3,19,2,2);
+insert into prodorden (id_orden,id_pts,cantidad) values (1,3,1);
+insert into prodorden (id_orden,id_pts,cantidad) values (1,4,2);
 
 insert into prodorden_topping (id_topping,id_productocreado) values (1,2);
 insert into prodorden_topping (id_topping,id_productocreado) values (2,2);
@@ -162,9 +159,9 @@ insert into prodorden_topping (id_topping,id_productocreado) values (3,2);
 
 
 -- querys
-select producto.tipo,tamano.nombre,subtipo.nombre,producto_tamano_subtipo.precio, pizza.num_toppings from producto_tamano_subtipo join tamano on producto_tamano_subtipo.id_tamano=tamano.id_tamano join producto on producto_tamano_subtipo.id_producto=producto.id_producto join subtipo on producto_tamano_subtipo.id_subtipo=subtipo.id_subtipo join pizza on producto_tamano_subtipo.id_subtipo=pizza.id_subtipo order by pizza.num_toppings asc; 
 
+select pro_tam_sub.id_pts, producto.tipo, subtipo.nombre, tamano.nombre, pro_tam_sub.precio from pro_tam_sub join subtipo on pro_tam_sub.id_subtipo = subtipo.id_subtipo join tamano on tamano.id_tamano = pro_tam_sub.id_tamano join producto on pro_tam_sub.id_producto = producto.id_producto;   
 
-select producto.tipo,prodorden_topping.id_topping,prodorden.id_orden,prodorden.id_subtipo,prodorden.id_tamano from prodorden join producto on producto.id_producto=prodorden.id_producto join prodorden_topping on prodorden.id_productoCreado = prodorden_topping.id_productoCreado order by prodorden.id_tamano asc;
+select A.id_pts, A.precio, subtipo.nombre "Productos variados", B.cantidad, ordenes.fecha "Fecha Solicitud", usuarios.nombre from pro_tam_sub A, subtipo, prodorden B, ordenes, usuarios where A.id_subtipo = subtipo.id_subtipo and B.id_pts = A.id_pts and ordenes.id_orden = B.id_orden and usuarios.id_usuario = ordenes.id_orden;
 
 
