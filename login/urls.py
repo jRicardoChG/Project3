f84da7b1a1b8dcd771a6341a2ccfdeb73f674460
Ricardo <richago29@gmail.com>
2019-11-24 21:30:06 -0500

    seguir confiugrando logica, el menu sin loguearse era la foto del menu pdf, y al loguearse obtiene acceso a solicitud de cosas

--- login/urls.py
+++ login/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="indexlogin"),
+    path("", views.login, name="indexlogin"),
     path("registro/",views.registro, name="registro")
 ]
--- login/views.py
+++ login/views.py
@@ -1,12 +1,26 @@
 import sys
 import os
 
-from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth import authenticate,login,logout
+from django.http import HttpResponse,Http404,HttpResponseRedirect, HttpRequest
 from django.shortcuts import render
+from django.urls import reverse
 
+def login(request):
+    if request.user.is_authenticated:
+        context = {"username":request.user}
+        return render(request, "orders/home.html",context)
+    else:
+        if request.method == "POST":
+            print("hola se que enviaste un post")
+        elif request.method == "GET":
+            print("hola se que enviaste un get")
+            return render(request,'login/login.html')
+        else:
+            return render(request,'login/login.html')
 
-def index(request):
-    return render(request,'login/login.html')
 
 def registro(request):
     return render(request,'login/registro.html')
+
+
