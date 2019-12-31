5332c0e3d010a6dbe4d72f2c38a2c882421a6460
Ricardo <richago29@gmail.com>
2019-12-31 11:46:01 -0500

    listo manejo de toppings, toca llenar los select con los toppings disponibles

--- static/javascript/javascript.js
+++ static/javascript/javascript.js
@@ -1,9 +1,14 @@
+
 var xhr = new XMLHttpRequest();
 var parser = new DOMParser();
 var menu = document.querySelector("#menu");
 var formElemento = document.querySelector("#formSubtipo");
 var pedirProd = document.querySelector("#pedirProd");
 var topps = document.querySelector("#toppings");
+var banderaCreado = false;
+var askNumTop1 = "</div><div class='flexFatherCol global'><label class='fontBold global'>Cuantos Toppings deseas?</label><input type='number' name='num_Toppings' id='num_Toppings' min='1' max='3' placeholder='#'/></div><div id='selTopFather'>";
+var askNumTop
+
 
 xhr.onreadystatechange = function(){
     if(xhr.readyState == 4)
@@ -57,6 +62,29 @@ function peticion(elemento)
     xhr.send("csrfmiddlewaretoken="+document.getElementsByName('csrfmiddlewaretoken')[0].value+"&producto="+elemento.innerHTML);
 }
 
+function crearEventosRadios()
+{       
+    var divSubTipo = document.querySelector("#subtipo");
+    divSubTipo.addEventListener("change",function(){
+        var prod1=document.querySelector("#Producto1");
+        var lprod1 = document.querySelector("#labProd1");
+        var prod3=document.querySelector("#Producto3");
+        var lprod3 = document.querySelector("#labProd3");
+        if(prod1.checked || prod3.checked || lprod1.checked || prod3.checked)
+        {
+            askNumTop.setAttribute("max",5);
+            askNumTop.value = 5;
+            askNumTop.dispatchEvent(eventoCambio);
+        }
+        else
+        {
+            askNumTop.setAttribute("max",3);
+            askNumTop.value = 3;
+            askNumTop.dispatchEvent(eventoCambio);
+        }
+    })
+}
+
 function llenarSubtipo(respuesta)
 {
     var cont = 0;
@@ -67,9 +95,10 @@ function llenarSubtipo(respuesta)
         var labl = document.createElement("label");
         rad.type = "radio";
         rad.name = "ProdSeleccionado";
-        rad.id = "Hola"+cont;
-        labl.setAttribute("for","Hola"+cont);
+        rad.id = "Producto"+cont;
+        labl.setAttribute("for","Producto"+cont);
         labl.innerHTML = valor;
+        labl.id = "labProd"+cont;
         rad.value = valor;
         divP.classList.add("flexFatherLeftRow","global","width100");
         rad.classList.add("flexItem","global");
@@ -81,26 +110,35 @@ function llenarSubtipo(respuesta)
     }
     if(respuesta["toppings"]!="false")
     {
-        var askNumTop1 = "<div class='flexFatherCol global'><label class='fontBold global'>Cuantos Toppings deseas?</label><input type='number' name='num_Toppings' id='num_Toppings' min='1' max='3' placeholder='1'/></div>";
         topps.innerHTML = askNumTop1;
         topps.parentNode.innerHTML += "<hr class='hr global'>";
         askNumTop = document.querySelector("#num_Toppings");
+        if(askNumTop)
+        {
+            crearEventosRadios();
+        }
+        if(!banderaCreado)
+        {
+            eventoCambio = new Event("change");
+            askNumTop.addEventListener("change",function(){
+                console.log("hola cambiaste");
+                document.querySelector("#selTopFather").innerHTML = "";
+                banderaCreado = true;
+                if(askNumTop.value=="")
+                    askNumTop.value = 1;
+                for(let i=0;i<askNumTop.value;i++)
+                {
+                    console.log("appden de "+(i+1)+" hijos");
+                    var seltopps = document.createElement("div");
+                    seltopps.innerHTML = "<select class='global form-control' id='selToppings"+(i+1)+"'><option value='hola'>Hola</option></select>";
+                    document.querySelector("#selTopFather").appendChild(seltopps);
+                }  
+            });
+        }
     }
     
 }
 
 
-document.addEventListener("change",function(){
-    if(askNumTop)
-    {
-        askNumTop.addEventListener("change",function(){
-            for(i=0;i<askNumTop.value;i++)
-            {
-            var topps = document.querySelector("#toppings");
-            var seltopps = "<div class='global'><select class='global form-control' id='selToppings"+i+"'><option value='hola'>Hola</option></select></div>";
-            topps.innerHTML += seltopps;
-            }  
-        });
-    }
-})
+
 
